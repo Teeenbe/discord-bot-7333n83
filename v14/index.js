9eeb56ab67b347333afb7391cfc71b08383d0200
Teeenbe <tomnbennett2013@gmail.com>
2023-04-23 11:36:49 +0100

    Event handling added

--- v14/index.js
+++ v14/index.js
@@ -1,7 +1,7 @@
 //*** MODULES ***//
 const fs = require("node:fs");
 const path = require("node:path");
-const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
 const token = process.env.TOKEN;
 
 //*** CLIENT INSTANTIATION ***//
@@ -33,44 +33,20 @@ for (const folder of commandFolders) {
     }
 }
 
-//*** CLIENT LOGIN ***//
-client.once(Events.ClientReady, (c) => {
-    console.log(`Booted! Logged in as ${c.user.tag}`);
-});
+//*** EVENT HANDLING ***//
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+    .readdirSync(eventsPath)
+    .filter((file) => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
 
 client.login(token);
-
-//*** COMMAND EXECUTION ***//
-client.on(Events.InteractionCreate, async (interaction) => {
-    if (!interaction.isChatInputCommand) {
-        return;
-    }
-
-    console.log(interaction);
-
-    const command = interaction.client.commands.get(interaction.commandName);
-
-    if (!command) {
-        console.error(
-            `No command matching ${interaction.commandName} was found.`
-        );
-        return;
-    }
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        if (interaction.replied || interaction.deferred) {
-            await interaction.followUp({
-                content: "There was an error while executing this command!",
-                ephemeral: true,
-            });
-        } else {
-            await interaction.reply({
-                content: "There was an error while executing this command!",
-                ephemeral: true,
-            });
-        }
-    }
-});
